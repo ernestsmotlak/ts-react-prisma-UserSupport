5f0e53a763c5450d2da7878e49f992ee6a167f2e
ernestsmotlak <ernestsmotlak@gmail.com>
2024-08-26 14:48:28 +0200

    Revert to the one before if problems.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- server/src/controllers/expenseController.ts
+++ server/src/controllers/expenseController.ts
@@ -59,7 +59,7 @@ export const addExpense = async (req: Request, res: Response) => {
     // Convert the values to the correct types
     const groupIdInt = parseInt(groupId, 10);
     const userIdInt = parseInt(userId, 10);
-    console.log('!! userIDint: ', userIdInt);
+    console.log("!! userIDint: ", userIdInt);
     const amountPaidFloat = parseFloat(amountPaid);
 
     // Check if the conversions are successful
@@ -85,3 +85,55 @@ export const addExpense = async (req: Request, res: Response) => {
       .json({ error: "An error occurred while adding the expense" });
   }
 };
+
+// export const updateExpense = async (req: Request, res: Response) => {
+//   const { expenseId } = req.params;
+//   const { groupName, paidById, amountPaid } = req.body;
+
+//   console.log(expenseId);
+//   console.log(groupName);
+//   console.log(paidById);
+//   console.log(amountPaid);
+
+//   try {
+//     const nameOfGroup = groupName;
+//     const idOfExpense = Number(expenseId);
+//     const idOfPayer = Number(paidById);
+//     const paidAmount = parseFloat(amountPaid);
+
+//     if (
+//       nameOfGroup === "" ||
+//       isNaN(idOfExpense) ||
+//       isNaN(idOfPayer) ||
+//       isNaN(paidAmount)
+//     ) {
+//       return res.status(400).json({ error: "Invalid input data!" });
+//     }
+
+//     const expenseUpdate = await prisma.expense.update({
+//       where: { id: idOfExpense },
+//       data: {
+//         group: {
+//           update: {
+//             name: groupName,
+//           },
+//         },
+//         paidBy: {
+//           connect: {
+//             id: idOfPayer,
+//           },
+//         },
+//       },
+//       include: {
+//         group: true,
+//       },
+//     });
+
+//     res.status(200).json({ expenseUpdate });
+//   } catch (error) {
+//     console.error("Error updating expense!", error);
+//     res
+//       .status(500)
+//       .json({ error: "An error occured while updating the expense!" });
+//   }
+// };
